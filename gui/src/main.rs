8610102f3896f4db5b25a0c90325f7f34a068bb7
Ruin321 <schooltaregf0000@outlook.com>
2026-07-27 01:51:18 +0800

    feat: load system CJK font for Chinese/Japanese/Korean display
    - Try Windows fonts: Microsoft YaHei, SimHei
    - Try Linux fonts: Noto Sans CJK
    - Fall back to default if no CJK font found

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,5 +1,52 @@
 mod app;
 
+fn load_cjk_fonts(ctx: &egui::Context) {
+    let mut fonts = egui::FontDefinitions::default();
+
+    // Try to load a CJK font from the system
+    let cjk_font_data = {
+        // Windows: Microsoft YaHei or SimHei
+        let win_paths = [
+            "C:/Windows/Fonts/msyh.ttc",
+            "C:/Windows/Fonts/simhei.ttf",
+            "C:/Windows/Fonts/msyhbd.ttc",
+        ];
+        // Linux: Noto Sans CJK
+        let linux_paths = [
+            "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc",
+            "/usr/share/fonts/noto-cjk/NotoSansCJK-Regular.ttc",
+            "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc",
+        ];
+
+        let paths: Vec<&str> = if cfg!(windows) {
+            win_paths.to_vec()
+        } else {
+            linux_paths.to_vec()
+        };
+
+        paths.iter().find_map(|p| std::fs::read(p).ok())
+    };
+
+    if let Some(data) = cjk_font_data {
+        fonts.font_data.insert(
+            "cjk".to_owned(),
+            egui::FontData::from_owned(data),
+        );
+        fonts
+            .families
+            .entry(egui::FontFamily::Proportional)
+            .or_default()
+            .insert(0, "cjk".to_owned());
+        fonts
+            .families
+            .entry(egui::FontFamily::Monospace)
+            .or_default()
+            .insert(0, "cjk".to_owned());
+    }
+
+    ctx.set_fonts(fonts);
+}
+
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
@@ -13,6 +60,7 @@ fn main() -> eframe::Result {
         "wrappe GUI",
         options,
         Box::new(|cc| {
+            load_cjk_fonts(&cc.egui_ctx);
             cc.egui_ctx.set_visuals(egui::Visuals::dark());
             Ok(Box::new(app::WrappeApp::default()))
         }),
