41852ec4b16ec323674fa04c2f132118ab3a7947
Ruin321 <schooltaregf0000@outlook.com>
2026-07-28 21:53:10 +0800

    fix: use push() instead of insert(0) for CJK font to avoid breaking default fonts

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,52 +1,5 @@
 mod app;
 
-fn load_cjk_fonts(ctx: &egui::Context) {
-    let mut fonts = egui::FontDefinitions::default();
-
-    // Try to load a CJK font from the system
-    let cjk_font_data = {
-        // Windows: Microsoft YaHei or SimHei
-        let win_paths = [
-            "C:/Windows/Fonts/msyh.ttc",
-            "C:/Windows/Fonts/simhei.ttf",
-            "C:/Windows/Fonts/msyhbd.ttc",
-        ];
-        // Linux: Noto Sans CJK
-        let linux_paths = [
-            "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc",
-            "/usr/share/fonts/noto-cjk/NotoSansCJK-Regular.ttc",
-            "/usr/share/fonts/truetype/noto/NotoSansCJK-Regular.ttc",
-        ];
-
-        let paths: Vec<&str> = if cfg!(windows) {
-            win_paths.to_vec()
-        } else {
-            linux_paths.to_vec()
-        };
-
-        paths.iter().find_map(|p| std::fs::read(p).ok())
-    };
-
-    if let Some(data) = cjk_font_data {
-        fonts.font_data.insert(
-            "cjk".to_owned(),
-            egui::FontData::from_owned(data),
-        );
-        fonts
-            .families
-            .entry(egui::FontFamily::Proportional)
-            .or_default()
-            .insert(0, "cjk".to_owned());
-        fonts
-            .families
-            .entry(egui::FontFamily::Monospace)
-            .or_default()
-            .insert(0, "cjk".to_owned());
-    }
-
-    ctx.set_fonts(fonts);
-}
-
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
@@ -60,7 +13,18 @@ fn main() -> eframe::Result {
         "wrappe GUI",
         options,
         Box::new(|cc| {
-            load_cjk_fonts(&cc.egui_ctx);
+            // Try to add CJK font support without breaking default fonts
+            let mut fonts = egui::FontDefinitions::default();
+            let cjk_data = std::fs::read("C:/Windows/Fonts/msyh.ttc")
+                .or_else(|_| std::fs::read("C:/Windows/Fonts/simhei.ttf"))
+                .or_else(|_| std::fs::read("/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc"))
+                .ok();
+            if let Some(data) = cjk_data {
+                fonts.font_data.insert("cjk".to_owned(), egui::FontData::from_owned(data));
+                fonts.families.entry(egui::FontFamily::Proportional).or_default().push("cjk".to_owned());
+                fonts.families.entry(egui::FontFamily::Monospace).or_default().push("cjk".to_owned());
+                cc.egui_ctx.set_fonts(fonts);
+            }
             cc.egui_ctx.set_visuals(egui::Visuals::dark());
             Ok(Box::new(app::WrappeApp::default()))
         }),
